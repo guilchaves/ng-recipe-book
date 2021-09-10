1d3b02485f5419278463d24948f3a396d4ee34a4
guilherme chaves <guilgchaves@protonmail.com>
2021-09-10 11:48:59 -0300

    chore(recipe-item): show item on recipe-list;

--- src/app/features/recipes/recipe-list/recipe-list.component.ts
+++ src/app/features/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Recipe } from 'src/app/core/models/recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test',
+      'This is a test recipe',
+      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'
+    ),
+    new Recipe(
+      'Test',
+      'This is a test recipe',
+      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'
+    ),
+  ]
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
