167d239e88b41029538bd8ebf077975281822a1a
guilherme chaves <guilgchaves@protonmail.com>
2021-09-11 16:07:34 -0300

    chore(component): shopping-edit added to codebase;

--- src/app/features/shopping-list/shopping-list.component.ts
+++ src/app/features/shopping-list/shopping-list.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Ingredient } from 'src/app/core/models/ingredient.model'
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.scss']
+  styleUrls: ['./shopping-list.component.scss'],
 })
 export class ShoppingListComponent implements OnInit {
+  public ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Shoyu', 3),
+  ]
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
