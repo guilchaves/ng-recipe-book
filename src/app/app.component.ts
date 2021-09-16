3e8dcae414e8d898452659fea1f799bb178561ec
guilherme chaves <guilgchaves@protonmail.com>
2021-09-16 11:29:13 -0300

    chore(navigation): provisional navigation added through event binding;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ng-recipe-book';
+  loadedFeature: string = 'recipe'
+
+  onNavigate(feature: string): void {
+    this.loadedFeature = feature
+  }
 }
--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, EventEmitter, OnInit, Output } from '@angular/core'
 
 @Component({
   selector: 'app-header',
@@ -6,8 +6,14 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  public collapsed: boolean = true
+  collapsed: boolean = true
+  @Output() featureSelected = new EventEmitter<string>()
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature)
+  }
 }
