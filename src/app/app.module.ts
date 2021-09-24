7d6ddc368d5261104138a289b9cf1204319227cd
guilherme <guilgchaves@protonmail.com>
2021-09-24 15:26:28 -0300

    refac(module): shopping-list module created;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,15 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
+import { RecipesModule } from './features/recipes/recipes.module'
+import { ShoppingListModule } from './features/shopping-list/shopping-list.module'
+
 import { AppComponent } from './app.component'
 import { HeaderComponent } from './core/components/header/header.component'
-import { ShoppingListComponent } from './features/shopping-list/shopping-list.component'
-import { ShoppingEditComponent } from './features/shopping-list/components/shopping-edit/shopping-edit.component'
-import { RecipesModule } from './features/recipes/recipes.module'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
-  ],
-  imports: [BrowserModule, AppRoutingModule, RecipesModule],
+  declarations: [AppComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule, RecipesModule, ShoppingListModule],
   providers: [],
   bootstrap: [AppComponent],
 })
