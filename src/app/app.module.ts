56df344ca5b548cb8ace56ae7b4d956615a078cf
guilherme <guilgchaves@protonmail.com>
2021-09-24 15:32:27 -0300

    refac(module): core module created;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
+import { CoreModule } from './core/core.module'
 import { RecipesModule } from './features/recipes/recipes.module'
 import { ShoppingListModule } from './features/shopping-list/shopping-list.module'
 
 import { AppComponent } from './app.component'
-import { HeaderComponent } from './core/components/header/header.component'
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, RecipesModule, ShoppingListModule],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RecipesModule,
+    ShoppingListModule,
+    CoreModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
