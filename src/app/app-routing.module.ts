050fdfd59db7e258679b7398f786ec941d8efb55
guilherme <guilgchaves@protonmail.com>
2021-10-25 10:25:03 -0300

    chore(router): set child routes for recipe path;

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { RecipeDetailComponent } from './features/recipes/components/recipe-detail/recipe-detail.component'
 
+import { RecipeStartComponent } from './features/recipes/components/recipe-start/recipe-start.component'
 import { RecipesComponent } from './features/recipes/recipes.component'
 import { ShoppingListComponent } from './features/shopping-list/shopping-list.component'
 
@@ -13,6 +15,10 @@ const routes: Routes = [
   {
     path: 'recipes',
     component: RecipesComponent,
+    children: [
+      { path: '', component: RecipeStartComponent },
+      { path: ':id', component: RecipeDetailComponent },
+    ],
   },
   {
     path: 'shopping-list',
--- src/app/features/recipes/recipes.module.ts
+++ src/app/features/recipes/recipes.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { SharedModule } from 'src/app/shared/shared.module'
+import { RouterModule } from '@angular/router'
 
 import { RecipeListComponent } from './components/recipe-list/recipe-list.component'
 import { RecipesComponent } from './recipes.component'
 import { RecipeDetailComponent } from './components/recipe-detail/recipe-detail.component'
 import { RecipeItemComponent } from './components/recipe-list/recipe-item/recipe-item.component'
+import { RecipeStartComponent } from './components/recipe-start/recipe-start.component'
 
 @NgModule({
   declarations: [
@@ -13,13 +15,15 @@ import { RecipeItemComponent } from './components/recipe-list/recipe-item/recipe
     RecipeListComponent,
     RecipeDetailComponent,
     RecipeItemComponent,
+    RecipeStartComponent,
   ],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, RouterModule],
   exports: [
     RecipesComponent,
     RecipeListComponent,
     RecipeDetailComponent,
     RecipeItemComponent,
+    RecipeStartComponent,
   ],
 })
 export class RecipesModule {}
