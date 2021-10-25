f13f23d031c267bfe1995fb5bbd6883cc578b98e
guilherme <guilgchaves@protonmail.com>
2021-10-25 10:41:27 -0300

    chore(router): configured child routes params;

--- src/app/features/recipes/components/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/features/recipes/components/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core'
 import { Recipe } from '../../../models/recipe.model'
-import { RecipeService } from '../../../services/recipe.service'
 
 @Component({
   selector: 'app-recipe-item',
@@ -10,11 +9,7 @@ import { RecipeService } from '../../../services/recipe.service'
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe
 
-  constructor(private recipeService: RecipeService) {}
+  constructor() {}
 
   ngOnInit(): void {}
-
-  onSelected() {
-    this.recipeService.recipeSelected.emit(this.recipe)
-  }
 }
