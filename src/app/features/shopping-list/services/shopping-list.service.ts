c6f55024bf30248e69a67492078d43e4a6fa0457
guilherme <guilgchaves@protonmail.com>
2021-11-04 15:49:49 -0300

    refac(subject): using subject instead of event emitter;

--- src/app/features/shopping-list/services/shopping-list.service.ts
+++ src/app/features/shopping-list/services/shopping-list.service.ts
@@ -1,11 +1,13 @@
-import { EventEmitter, Injectable } from '@angular/core'
+import { Injectable } from '@angular/core'
+import { Subject } from 'rxjs'
+
 import { Ingredient } from '../models/ingredient.model'
 
 @Injectable({
   providedIn: 'root',
 })
 export class ShoppingListService {
-  ingredientsChanged = new EventEmitter<Ingredient[]>()
+  ingredientsChanged = new Subject<Ingredient[]>()
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
@@ -20,11 +22,11 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient)
-    this.ingredientsChanged.emit(this.ingredients.slice())
+    this.ingredientsChanged.next(this.ingredients.slice())
   }
 
   addIngredients(ingredients: Ingredient[]) {
     this.ingredients.push(...ingredients)
-    this.ingredientsChanged.emit(this.ingredients.slice())
+    this.ingredientsChanged.next(this.ingredients.slice())
   }
 }
