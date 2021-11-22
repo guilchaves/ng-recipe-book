5b8c1929886db6104b9370ccabaa1d3a6e06638b
guilherme <guilgchaves@protonmail.com>
2021-11-22 14:15:35 -0300

    chore(forms): user can delete items

--- src/app/features/shopping-list/components/shopping-edit/shopping-edit.component.ts
+++ src/app/features/shopping-list/components/shopping-edit/shopping-edit.component.ts
@@ -72,6 +72,11 @@ export class ShoppingEditComponent implements OnInit {
     form.reset()
   }
 
+  onDelete() {
+    this.shoppingListService.deleteIngredient(this.editedItemIndex)
+    this.onClear()
+  }
+
   onClear() {
     this.slForm.reset()
     this.editMode = false
