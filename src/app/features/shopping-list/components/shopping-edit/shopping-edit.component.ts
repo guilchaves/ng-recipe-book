c6e1ee3f5073634c41b99193655953030777ace5
guilherme <guilgchaves@protonmail.com>
2021-11-22 13:47:50 -0300

    chore(forms): loading sl items into the form;

--- src/app/features/shopping-list/components/shopping-edit/shopping-edit.component.ts
+++ src/app/features/shopping-list/components/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,11 @@
-import { Component, ElementRef, EventEmitter, OnInit, Output } from '@angular/core'
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core'
 import { NgForm } from '@angular/forms'
 import { Subscription } from 'rxjs'
 import { Ingredient } from '../../models/ingredient.model'
@@ -20,15 +27,22 @@ export class ShoppingEditComponent implements OnInit {
   //@Output() ingredientAdded = new EventEmitter<Ingredient>()
 
   constructor(private shoppingListService: ShoppingListService) {}
+  @ViewChild('f') slForm: NgForm
   subscription: Subscription
   editMode = false
   editedItemIndex: number
+  editedItem: Ingredient
 
   ngOnInit(): void {
     this.subscription = this.shoppingListService.startedEditing.subscribe(
       (index: number) => {
         this.editedItemIndex = index
         this.editMode = true
+        this.editedItem = this.shoppingListService.getIngredient(index)
+        this.slForm.setValue({
+          name: this.editedItem.name,
+          amount: this.editedItem.amount,
+        })
       }
     )
   }
