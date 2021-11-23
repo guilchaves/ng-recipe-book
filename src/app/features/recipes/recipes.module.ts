97e83d708293be381a6150a16980ae25b7157380
guilherme <guilgchaves@protonmail.com>
2021-11-23 14:44:22 -0300

    chore(recipe-edit): adding image preview;

--- src/app/features/recipes/recipes.module.ts
+++ src/app/features/recipes/recipes.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { SharedModule } from 'src/app/shared/shared.module'
 import { RouterModule } from '@angular/router'
+import { ReactiveFormsModule } from '@angular/forms'
 
-import { RecipeListComponent } from './components/recipe-list/recipe-list.component'
 import { RecipesComponent } from './recipes.component'
 import { RecipeDetailComponent } from './components/recipe-detail/recipe-detail.component'
+import { RecipeEditComponent } from './components/recipe-edit/recipe-edit.component'
+import { RecipeListComponent } from './components/recipe-list/recipe-list.component'
 import { RecipeItemComponent } from './components/recipe-list/recipe-item/recipe-item.component'
 import { RecipeStartComponent } from './components/recipe-start/recipe-start.component'
-import { RecipeEditComponent } from './components/recipe-edit/recipe-edit.component'
-import { ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
