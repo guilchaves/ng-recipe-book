b0b43d59948a790c2fcdfc6e41f5b2251d46876e
guilherme <guilgchaves@protonmail.com>
2021-11-23 14:55:58 -0300

    fix(header): fixed dropdown from manage menu;

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { HeaderComponent } from './components/header/header.component'
 import { AppRoutingModule } from '../app-routing.module'
+import { SharedModule } from '../shared/shared.module'
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, AppRoutingModule],
+  imports: [CommonModule, AppRoutingModule, SharedModule],
   exports: [HeaderComponent],
 })
 export class CoreModule {}
