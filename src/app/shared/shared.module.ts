5c3d574859d696c6d132175e750f2b3e44f68879
guilherme <guilgchaves@protonmail.com>
2021-12-13 13:50:41 -0300

    feat: create data-storage service

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+
 import { DropdownDirective } from './directives/dropdown.directive'
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [DropdownDirective],
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   exports: [DropdownDirective],
 })
 export class SharedModule {}
