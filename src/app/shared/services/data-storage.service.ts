a5c14084a5a8b913c52853e987ee1862c541720e
guilherme <guilgchaves@protonmail.com>
2021-12-13 14:16:59 -0300

    feat: fetch recipes from db

--- src/app/shared/services/data-storage.service.ts
+++ src/app/shared/services/data-storage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 
+import { Recipe } from 'src/app/features/recipes/models/recipe.model'
 import { RecipeService } from 'src/app/features/recipes/services/recipe.service'
 
 @Injectable({ providedIn: 'root' })
@@ -16,6 +17,16 @@ export class DataStorageService {
       )
       .subscribe(resp => {
         console.log(resp)
+      })
+  }
+
+  fetchRecipes() {
+    this.http
+      .get<Recipe[]>(
+        'https://ng-recipe-book-e5639-default-rtdb.firebaseio.com/recipes.json'
+      )
+      .subscribe(recipes => {
+        this.recipeService.setRecipes(recipes)
       })
   }
 }
