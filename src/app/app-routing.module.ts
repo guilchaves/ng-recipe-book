76e224b94615ae1d87fa90ace3686044a14db5fc
guilherme <guilgchaves@protonmail.com>
2021-12-14 13:54:18 -0300

    feat: add authentication page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
+import { AuthComponent } from './core/components/auth/auth.component'
 import { RecipeDetailComponent } from './features/recipes/components/recipe-detail/recipe-detail.component'
 import { RecipeEditComponent } from './features/recipes/components/recipe-edit/recipe-edit.component'
 import { RecipeStartComponent } from './features/recipes/components/recipe-start/recipe-start.component'
@@ -35,6 +36,10 @@ const routes: Routes = [
   {
     path: 'shopping-list',
     component: ShoppingListComponent,
+  },
+  {
+    path: 'auth',
+    component: AuthComponent,
   },
 ]
 
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { HeaderComponent } from './components/header/header.component'
 import { AppRoutingModule } from '../app-routing.module'
 import { SharedModule } from '../shared/shared.module'
 
+import { HeaderComponent } from './components/header/header.component'
+import { AuthComponent } from './components/auth/auth.component'
+
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, AuthComponent],
   imports: [CommonModule, AppRoutingModule, SharedModule],
   exports: [HeaderComponent],
 })
